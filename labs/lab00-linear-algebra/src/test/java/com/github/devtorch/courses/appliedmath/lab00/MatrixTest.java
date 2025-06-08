0cd1a3a68668fbf175a168c1473e55a5a4460999
Vitaliy <torchez@yandex.ru>
2025-06-08 06:29:51 +0300

    **[Week00]** Базовая конфигурация проекта, обновление описаний, первые классы тестов

---
+++ labs/lab00-linear-algebra/src/test/java/com/github/devtorch/courses/appliedmath/lab00/MatrixTest.java
@@ -0,0 +1,27 @@
+package com.github.devtorch.courses.appliedmath.lab00;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatrixTest {
+
+    @Test void mulVector() {
+        Matrix A = new DenseMatrix(new double[][]{
+                {1, 2, 0},
+                {0, 1, -1}
+        });
+        Vector x = Vector.of(1, 1, 1);
+        Vector y = A.mul(x);
+        assertNotNull(y);
+        assertArrayEquals(new double[]{3, 0}, y.toArray(), 1e-9);
+    }
+
+    @Test void transpose() {
+        Matrix A = new DenseMatrix(new double[][]{ {0, 1}, {2, 3} });
+        Matrix AT = A.T();
+        assertNotNull(AT);
+        assertEquals(0, AT.at(0,0));
+        assertEquals(2, AT.at(1,0));
+        assertEquals(1, AT.at(0,1));
+    }
+}
